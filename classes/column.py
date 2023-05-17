f629e524c484353ec184fe45d8d5e87ad0fde84a
ove4lo <alina.mik.kz@gmail.com>
2023-05-17 17:54:59 +1000

    Create functions for DataBase and config file

---
+++ classes/column.py
@@ -0,0 +1,7 @@
+class column:
+    id = None
+    board_id = None
+
+    def __init__(self, id, board_id):
+        self.id = id
+        self.board_id = board_id
---
+++ classes/task.py
@@ -0,0 +1,17 @@
+class task:
+    id = None
+    title = None
+    description = None
+    checkbox = None
+    color = None
+    photos = None
+    column_id = None
+
+    def __init__(self, id, title, description, color, checkbox, photos, column_id):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.color = color
+        self.checkbox = checkbox
+        self.photos = photos
+        self.column_id = column_id
