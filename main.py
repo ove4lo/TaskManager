a5cafef718e1a6bf9fa486488b0d9885b3ff72ec
ove4lo <alina.mik.kz@gmail.com>
2023-05-29 20:51:45 +1000

    login str

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from classes.database import DB
 db = DB()
 
 # db.add_user("6ilya", "sokola04", "Ilya", "Sokolov")
-print(db.authorized_user("6ilya", "sokola04"))
+# print(db.authorized_user("6ilya", "sokola04"))
